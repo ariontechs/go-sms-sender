2df664e1ad421a9e18df297bbd08be3569e4ff70
ariontechs <nat@ariontechs.com>
2023-07-19 13:40:28 +0800

    fix: remove log

--- infobip.go
+++ infobip.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/http/httputil"
 	"strings"
 )
 
@@ -40,7 +39,6 @@ type Destination struct {
 }
 
 func GetInfobipClient(sender string, appKey string, other []string) (*InfobipClient, error) {
-	fmt.Println("GetInfobipClient")
 	return &InfobipClient{
 		baseUrl:other[0],
 		sender:sender,
@@ -68,12 +66,7 @@ func (c *InfobipClient) SendMessage(param map[string]string, targetPhoneNumber .
 		mobile = mobile[1:]
 	}
 
-	fmt.Println("mbile:"+mobile )
-
-	
 	endpoint := fmt.Sprintf("%s/sms/2/text/advanced", c.baseUrl)
-	fmt.Println("endpoint: "+ endpoint)
-
 	messageData := MessageData{
 		Messages: []Message{
 			{
@@ -92,35 +85,23 @@ func (c *InfobipClient) SendMessage(param map[string]string, targetPhoneNumber .
 		"Content-Type":  "application/json",
 	}
 
-	fmt.Println("headers: ", headers)
-
-	
-
 	messageDataBytes, _ := json.Marshal(messageData)
-	fmt.Println("messageDataBytes: ", messageDataBytes)
 	req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(messageDataBytes))
 	for key, value := range headers {
 		req.Header.Set(key, value)
 	}
 
-	dump, err := httputil.DumpRequestOut(req, true)
-  if err != nil {
-    return nil
-  }
-
-
-
-  fmt.Printf("%s\n", dump)
+	// dump, err := httputil.DumpRequestOut(req, true)
+  // if err == nil {
+  //   fmt.Printf("%s\n", dump)
+  // }
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("sms error -> ", err)
-		panic(err)
+		return err
 	}
 	defer resp.Body.Close()
-
-	fmt.Println("resp:" + resp.Status)
 	return  nil
 }
 
